29da469466bdbd4e1c7543a796b2b8e6f6bc3e69
Yeimy <dmoya844@gmail.com>
2024-12-17 01:57:29 -0500

    Actualizacion base mongo

--- config/database.js
+++ config/database.js
@@ -2,11 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
     try {
-<<<<<<< HEAD
         const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://admin:pilatunas@cluster0.oq7cr.mongodb.net/pilatunas_db');
-=======
-        const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://julianhomezdev:M1oDnsbIo4HJ37a6@cluster0.kwqwh.mongodb.net/pilatunas_db');
->>>>>>> 73639e3310ccb3bc2cddfb5115ea6545590a94df
         console.log(`MongoDB conectado: ${conn.connection.host}`);
         
         mongoose.connection.on('error', (err) => {
